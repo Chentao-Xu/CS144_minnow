227ffce810b9e21820f07b2419e08e152c770328
CedricX <ct.xu@outlook.com>
2025-03-09 19:57:09 +0800

    implement warpping

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,25 @@
 #include "wrapping_integers.hh"
-#include "debug.hh"
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>( n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  static constexpr uint64_t delta = 1ULL << 32 ;
+
+  const uint64_t abs_base_seqno = raw_value_ - zero_point.raw_value_ ;
+  const uint64_t checkpoint_low =  checkpoint & 0xFFFFFFFFULL;
+  const uint64_t candidate = ( checkpoint & ~0xFFFFFFFFULL ) | abs_base_seqno;
+
+  if ( candidate >= delta && abs_base_seqno > checkpoint_low && ( abs_base_seqno - checkpoint_low ) > ( delta / 2 ) ) {
+    return candidate - delta;
+  }
+  if ( candidate < ~0xFFFFFFFFULL && checkpoint_low > abs_base_seqno && ( checkpoint_low - abs_base_seqno ) > ( delta / 2 ) ) {
+    return candidate + delta;
+  }
+  return candidate;
 }
