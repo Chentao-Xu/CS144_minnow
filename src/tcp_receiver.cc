dd6679620784b487091c976df6a5d033e199a720
CedricX <ct.xu@outlook.com>
2025-03-09 19:57:26 +0800

    implement tcp_receiver & pass check2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,47 @@
 #include "tcp_receiver.hh"
-#include "debug.hh"
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+  if ( writer().has_error() ) {
+    return;
+  }
+
+  if ( message.RST ) {
+    reader().set_error();
+    return;
+  }
+
+  if ( message.SYN && !isn_.has_value() ) {
+    isn_ = message.seqno;
+  }
+
+  if ( !isn_.has_value() ) {
+    return;
+  }
+
+  uint64_t abs_seqno = message.seqno.unwrap( isn_.value(), writer().bytes_pushed() + 1 );
+  if ( !message.SYN ) {
+    abs_seqno -= 1; // SYN占一位
+  }
+  reassembler_.insert( abs_seqno, move( message.payload ), message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+  TCPReceiverMessage msg;
+
+  if ( isn_.has_value() ) {
+    uint64_t next_seq = reassembler_.writer().bytes_pushed() + 1;
+    if ( reassembler_.writer().is_closed() ) { // FIN占一位
+      next_seq += 1;
+    }
+    msg.ackno = Wrap32::wrap( next_seq, isn_.value() );
+  }
+  msg.window_size
+    = static_cast<uint16_t>( min<uint64_t>( UINT16_MAX, reassembler_.writer().available_capacity() ) );
+  msg.RST = writer().has_error();
+
+  return msg;
 }
